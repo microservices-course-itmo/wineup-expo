6c2c15aacbbec04abd0cb62170680e19055cfbfe
Alex Strive <alexstrive@pm.me>
2020-09-04 10:54:19 +0300

    Feature: ESLint integration (#1)
    * fix(package.json): move declaration of @types/jest devDependency
    
    * feature(eslint): integrate eslint

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react-native'
+import { render } from '@testing-library/react-native'
 
 import App from '../App'
 
 describe('<App />', () => {
   it('has 1 child', () => {
-    const tree = render(<App />)
+    render(<App />)
   })
 })
