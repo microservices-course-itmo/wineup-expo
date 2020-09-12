9bf1976f3e4513e987d670e0b333329174fc5a0f
Alex Strive <alexstrive@pm.me>
2020-09-13 00:06:37 +0300

    feature(react-navigation): React-Navigation integration (#3)

---
+++ src/screens/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SettingsScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+  )
+}
+
+export default SettingsScreen
