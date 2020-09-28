5f1013621992fe544545446d8168af71eee4bc9a
Lisa <psycho000reaper@gmail.com>
2020-09-28 15:43:18 +0300

    rename functions and add JSDoc comments

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,25 +1,40 @@
-/** function latinCheck*/
-// no cyrillic letters allowed
-function latinCheck(string: string): boolean {
-    const cyrillic = /.*[а-яА-ЯёЁ]/g;
+/** @function
+ * @name containsLatinOnly
+ * Returns true if string doesn't contain cyrillic chars
+ *
+ * @param {string} string - string to check
+ */
+function containsLatinOnly(string: string): boolean {
+  const cyrillic = /.*[а-яА-ЯёЁ]/g
 
-    return string.search(cyrillic) === -1;
-  }
+  return string.search(cyrillic) === -1
+}
 
-  /** function emailCheck*/
-  // email must contain latin letters, numbers, dot, gap and dash. domain has at least 2 letters.
-  // parts of email are separated with @ sign
-  export function emailCheck(email: string): boolean {
-    const emailRegexp = /([a-zA-Z0-9\._-]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z]{2,})/;
+/** @function
+ * @name isEmail
+ * Returns true if string is a correct email
+ *
+ * An email contains latin letters, numbers, dot, gap, dash symbols.
+ * Domain has at least 2 letters. The @ sign is necessary.
+ *
+ * @param {string} email - string to check
+ * */
+export function isEmail(email: string): boolean {
+  const emailRegexp = /([a-zA-Z0-9\._-]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z]{2,})/
 
-    return email.search(emailRegexp) === 0 && latinCheck(email);
-  }
+  return email.search(emailRegexp) === 0 && containsLatinOnly(email)
+}
 
-/** function passwordCheck*/
-  // password must contain at least 8 symbols, at least 1 upper case letter or at least 1 numeric.
-  export function passwordCheck(password: string): boolean {
-    const passwordRegexp = /(?:.*[^А-Яа-яЁё]*)(?=^.{8,}$)(?=.*[a-z])((?=.*[0-9])|(?=.*[A-Z]))/;
+/** @function
+ * @name isAllowedPassword
+ * Returns true if string is a password which passes all the tests.
+ *
+ * An allowed password must contain at least 8 symbols, at least one uppercase letter or at least 1 numeric
+ *
+ * @param {string} password - string to check
+ * */
+export function isAllowedPassword(password: string): boolean {
+  const passwordRegexp = /(?:.*[^А-Яа-яЁё]*)(?=^.{8,}$)(?=.*[a-z])((?=.*[0-9])|(?=.*[A-Z]))/
 
-    return password.search(passwordRegexp) === 0;
-  }
-  
+  return password.search(passwordRegexp) === 0
+}
