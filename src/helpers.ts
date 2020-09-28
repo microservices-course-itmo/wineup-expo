b294b3ee31c6df7d9471b7b67ba4fcd9c6979104
George Berezhnoy <gohabereg@gmail.com>
2020-09-28 23:02:46 +0300

    Fix ESLint

--- src/helpers.ts
+++ src/helpers.ts
@@ -20,7 +20,7 @@ function containsLatinOnly(string: string): boolean {
  * @param {string} email - string to check
  * */
 export function isEmail(email: string): boolean {
-  const emailRegexp = /([a-zA-Z0-9\._-]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z]{2,})/
+  const emailRegexp = /([a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,})/
 
   return email.search(emailRegexp) === 0 && containsLatinOnly(email)
 }
