ac704912c48688c3746ab733eeac61fc33108808
borwi4ok <donnos98@gmail.com>
2020-09-29 19:00:59 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from './screens/Home'
@@ -12,6 +13,7 @@ const App: React.FC = () => {
       <Tab.Navigator>
         <Tab.Screen name='Home' component={HomeScreen} />
         <Tab.Screen name='Settings' component={SettingsScreen} />
+        <Text>test </Text>
       </Tab.Navigator>
     </NavigationContainer>
   )
