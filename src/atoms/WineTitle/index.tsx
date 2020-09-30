c44a1b64562edfc55f356fca44623fa57b0c2fff
izukovdariy <42969746+izukovdariy@users.noreply.github.com>
2020-09-30 19:52:40 +0300

    Apply suggestions from code review
    Co-authored-by: George Berezhnoy <gohabereg@users.noreply.github.com>

--- src/atoms/WineTitle/index.tsx
+++ src/atoms/WineTitle/index.tsx
@@ -5,14 +5,13 @@ export interface WineTitleProps {
   name: string
   description: string
   volume: number
-  price?: number
 }
 
 const styles = StyleSheet.create({
   wineName: { fontSize: 20, fontWeight: 'bold', display: 'flex' },
 })
 
-function WineTitle({ name, description, volume }: Wine): ReactElement<Wine> {
+function WineTitle({ name, description, volume }: WineTitleProps): ReactElement<Wine> {
   return (
     <View>
       <Text style={styles.wineName}>{name}</Text>
