3fcf55a5a5f20afac6daae57bd2be4423a59d9ba
Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
2020-09-30 19:37:45 +0300

    fix code style

--- src/atoms/WineRating/index.tsx
+++ src/atoms/WineRating/index.tsx
@@ -3,27 +3,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 
 export interface WineRatingProps {
-  rating?: Number
-}
-
-export function WineRatingComponent({ rating = 3 }: WineRatingProps) {
-  return (
-    <View>
-      <Text>Оценка экспертов:</Text>
-      <View style={styles.container}>
-        {new Array(5).fill(0).map((_, index) => {
-          return (
-            <FontAwesome
-              key={index}
-              name={index + 1 > rating ? 'star-o' : 'star'}
-              size={24}
-              color='black'
-            />
-          )
-        })}
-      </View>
-    </View>
-  )
+  rating?: number | 0
 }
 
 const styles = StyleSheet.create({
@@ -40,4 +20,22 @@ const styles = StyleSheet.create({
   },
 })
 
-export default WineRatingComponent
+export default function ({ rating = 3 }: WineRatingProps) {
+  return (
+    <View>
+      <Text>Оценка экспертов:</Text>
+      <View style={styles.container}>
+        {new Array(5).fill(0).map((star, index) => {
+          return (
+            <FontAwesome
+              key={star}
+              name={index + 1 > rating ? 'star-o' : 'star'}
+              size={24}
+              color='black'
+            />
+          )
+        })}
+      </View>
+    </View>
+  )
+}
