606f3ad173dcd2d0086428792903e147d58c728c
ld50eli <elisey.minhairov@mail.ru>
2020-10-10 08:39:58 +0300

    Fixed style placement

--- src/atoms/WineShopName/index.tsx
+++ src/atoms/WineShopName/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import { View, Image, Text, StyleSheet } from 'react-native'
 
+interface ComponentProps {
+  image: string
+  name: string
+  description: string
+}
+
+const WineShopName = ({ image, name, description }: ComponentProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.flexedContainer}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={image} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.description}>{description}</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     width: 270,
@@ -39,27 +61,5 @@ const styles = StyleSheet.create({
     fontSize: 12,
   },
 })
-
-interface ComponentProps {
-  image: string
-  name: string
-  description: string
-}
-
-const WineShopName = ({ image, name, description }: ComponentProps) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.flexedContainer}>
-        <View style={styles.imageContainer}>
-          <Image style={styles.image} source={image} />
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.name}>{name}</Text>
-          <Text style={styles.description}>{description}</Text>
-        </View>
-      </View>
-    </View>
-  )
-}
 
 export default WineShopName
