2fef10cb4b214d693870a481279d9e9e1cb71d79
borwi4ok <donnos98@gmail.com>
2020-10-12 00:42:20 +0300

    create atom-result-value

--- src/atoms/advice/index.tsx
+++ src/atoms/advice/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
-function rightBordersHandler(value: number): number {
+export function correctValueHandler(value: number): number {
   let correctVal = value
   if (value > 100) correctVal = 100
   if (value < 0) correctVal = 0
@@ -11,7 +11,7 @@ function rightBordersHandler(value: number): number {
 const Advice: React.FunctionComponent<{ ratioAdvice: number }> = ({
   ratioAdvice,
 }) => {
-  const displayData = `Подходит Вам на ${rightBordersHandler(ratioAdvice)}%`
+  const displayData = `Подходит Вам на ${correctValueHandler(ratioAdvice)}%`
   return (
     <View style={styles.adviceContainer} testID='advice-test'>
       <Text>{displayData}</Text>
