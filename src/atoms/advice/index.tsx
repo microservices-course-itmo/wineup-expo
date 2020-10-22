8b2c11e25a69550a627ff882dc56dc4c260cfc75
borwi4ok <donnos98@gmail.com>
2020-10-22 03:17:41 +0300

    style-fix atom-advice

--- src/atoms/advice/index.tsx
+++ src/atoms/advice/index.tsx
@@ -3,8 +3,10 @@ import { Text, View, StyleSheet } from 'react-native'
 
 function rightBordersHandler(value: number): number {
   let correctVal = value
+
   if (value > 100) correctVal = 100
   if (value < 0) correctVal = 0
+
   return correctVal
 }
 
@@ -12,6 +14,7 @@ const Advice: React.FunctionComponent<{ ratioAdvice: number }> = ({
   ratioAdvice,
 }) => {
   const displayData = `Подходит Вам на ${rightBordersHandler(ratioAdvice)}%`
+
   return (
     <View style={styles.adviceContainer} testID='advice-test'>
       <Text>{displayData}</Text>
