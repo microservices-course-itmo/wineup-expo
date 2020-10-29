a9ed123ffbd5d3189ca8cd316a9e6530b171aed8
CleanBread <37740558+CleanBread@users.noreply.github.com>
2020-10-29 21:10:39 +0300

    wine rating component (#14)
    * wine rating component
    
    * fix code style
    
    * Fix
    
    Co-authored-by: Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
    Co-authored-by: George Berezhnoy <gohabereg@gmail.com>

---
+++ src/atoms/WineRating/index.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+
+import WineRatingComponent from './index'
+
+describe('<WineRatingComponent />', () => {
+  it('should render', () => {
+    render(<WineRatingComponent />)
+  })
+})
