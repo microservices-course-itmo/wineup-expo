a4c2137c3a8aecef30d35d95d6da233b6a7476a9
George Berezhnoy <gohabereg@gmail.com>
2020-11-29 22:09:59 +0300

    Add placeholder to favourites screen

--- src/molecules/FavoritesPlaceholder/index.tsx
+++ src/molecules/FavoritesPlaceholder/index.tsx
@@ -1,82 +1,73 @@
-import React, { useState } from 'react'
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
-import image from './src/arrow.png'
+import React from 'react'
+import styled from 'styled-components/native'
+import { useNavigation } from '@react-navigation/native'
+import image from '../../../assets/arrow.png'
+import PrimaryButton from '../../atoms/PrimaryButton'
 
-const FavoritesPlaceholder = () => {
-  const [isPressed, setPressed] = useState(false)
+function FavouritesPlaceholder() {
+  const navigation = useNavigation()
+
+  const navigate = () => {
+    navigation.navigate('Каталог')
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Тут пока пусто</Text>
-      <Text style={styles.flavorText}>
+    <Container>
+      <Heading>Тут пока пусто</Heading>
+      <Description>
         Наш каталог поможет Вам{'\n'} что-нибудь найти..
-      </Text>
-      <Image style={styles.arrow} source={image} />
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={1}
-        onPressIn={() => setPressed(true)}
-        onPressOut={() => setPressed(false)}
-      >
-        <Text
-          style={[styles.buttonText, isPressed && styles.buttonTextPressed]}
-        >
-          Перейти в каталог
-        </Text>
-      </TouchableOpacity>
-    </View>
+      </Description>
+      <Arrow source={image} />
+      <Button onPress={navigate}>
+        <ButtonLabel>Перейти в каталог</ButtonLabel>
+      </Button>
+    </Container>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
+const Container = styled.View`
+  position: relative;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 20px 150px;
+`
 
-  button: {
-    width: 250,
-    height: 70,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#931332',
-    borderRadius: 2,
-  },
+const Heading = styled.Text`
+  font-size: 25px;
+  font-family: PlayfairDisplay_400Regular;
+  text-align: center;
+  margin-bottom: 20px;
+`
 
-  text: {
-    fontSize: 25,
-    fontFamily: 'PlayfairDisplay_400Regular',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
+const Description = styled.Text`
+  font-family: Roboto_300Light;
+  font-weight: 300;
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 50px;
+  color: #707070;
+`
 
-  buttonText: {
-    fontSize: 18,
-    color: '#ffffff',
-  },
+const Button = styled(PrimaryButton)`
+  width: 250px;
+  height: 70px;
+  padding: 20px;
+`
 
-  buttonTextPressed: {
-    opacity: 0.25,
-  },
+const ButtonLabel = styled.Text`
+  font-size: 18px;
+  color: #ffffff;
+`
 
-  flavorText: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginBottom: 50,
-    color: '#707070',
-  },
+const Arrow = styled.Image`
+  position: absolute;
+  height: 60px;
+  width: 25px;
+  right: 20px;
+  top: 50%;
+  margin-top: 25px;
+`
 
-  arrow: {
-    height: 60,
-    width: 25,
-    position: 'absolute',
-    right: -20,
-    bottom: 68,
-  },
-})
-
-export default FavoritesPlaceholder
+export default FavouritesPlaceholder
