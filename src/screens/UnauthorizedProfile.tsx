a25a9132dd7b34ecec741bea0b54382e9cb79128
Maxim <maxim.bashilov@yandex.ru>
2021-02-16 17:33:07 +0300

    first screen
    add fixes for modal window, remove <Madal> element because it will not with our navbar menu.

--- src/screens/UnauthorizedProfile.tsx
+++ src/screens/UnauthorizedProfile.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Modal, View, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
 import styled from 'styled-components/native'
 import PrimaryButton from '../atoms/PrimaryButton'
 
-const StyledView = styled.View`
-  width: 100%;
-  height: 100%;
-  display: flex;
-`
-const StyledModalView = styled.View`
+const UnauthorizedProfile = () => (
+  <Container style={styles.modalView}>
+    <StyledText>Вы ещё не вошли в аккаунт</StyledText>
+    <StyledButton>
+      <ButtonLabel>ВОЙТИ В АККАУНТ</ButtonLabel>
+    </StyledButton>
+  </Container>
+)
+
+const styles = StyleSheet.create({
+  modalView: {
+    margin: 0,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 9,
+  },
+})
+
+const Container = styled.View`
   width: 340px;
   height: 200px;
   background: #ffffff;
@@ -25,7 +43,7 @@ const StyledText = styled.Text`
   line-height: 15px;
 `
 
-const StyledButtonText = styled.Text`
+const ButtonLabel = styled.Text`
   font-size: 14px;
   color: #ffffff;
 `
@@ -35,39 +53,8 @@ const StyledButton = styled(PrimaryButton)`
   height: 55px;
   border-radius: 5px;
   margin-top: 40px;
-  color: #fff;
+  color: #ffffff;
+  padding: 0;
 `
-
-const UnauthorizedProfile = () => {
-  const modalVisible = true
-
-  return (
-    <StyledView>
-      <Modal animationType='slide' transparent visible={modalVisible}>
-        <View>
-          <StyledModalView style={styles.modalView}>
-            <StyledText>Вы ещё не вошли в аккаунт</StyledText>
-            <StyledButton>
-              <StyledButtonText>ВОЙТИ В АККАУНТ</StyledButtonText>
-            </StyledButton>
-          </StyledModalView>
-        </View>
-      </Modal>
-    </StyledView>
-  )
-}
-
-const styles = StyleSheet.create({
-  modalView: {
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 9,
-  },
-})
 
 export default UnauthorizedProfile
