3e4ff701c510f7836fef77fc8f3eaff9221ca38a
Georgy Berezhnoy <gohabereg@gmail.com>
2021-02-24 22:55:38 +0300

    Remove storyloader from ignore

---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/atoms/Checkbox/index.stories')
+  require('../src/atoms/Like/index.stories')
+}
+
+const stories = [
+  '../src/atoms/Checkbox/index.stories',
+  '../src/atoms/Like/index.stories',
+]
+
+module.exports = {
+  loadStories,
+  stories,
+}
