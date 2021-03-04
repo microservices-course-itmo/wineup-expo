702afc7b4e3ad1747e8921b4bb9c60f34826699f
Georgy Berezhnoy <gohabereg@gmail.com>
2021-03-04 19:00:12 +0300

    Add Expo notifications registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import {
   useFonts,
@@ -22,13 +22,51 @@ import { AppLoading } from 'expo'
 import { SafeAreaView, StatusBar } from 'react-native'
 import { CacheProvider } from 'rest-hooks'
 import { MockProvider } from '@rest-hooks/test'
+import * as Notifications from 'expo-notifications'
+import { Subscription } from '@unimodules/core'
 import AuthWrapper from './screens/Auth/AuthWrapper'
 import { AuthProvider } from './screens/Auth/AuthContext'
 import MainRouter from './screens/Router'
 import { fixtures } from './tests/__mocks__/fixtures'
+import { registerForPushNotificationsAsync } from './utils/notifications'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+})
 
 const App: React.FC = () => {
   const [isAuth, setIsAuth] = useState(true)
+  const notificationListener = useRef<Subscription>()
+  const responseListener = useRef<Subscription>()
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log(notification)
+      }
+    )
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response)
+      }
+    )
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current as Subscription
+      )
+      Notifications.removeNotificationSubscription(
+        responseListener.current as Subscription
+      )
+    }
+  }, [])
 
   const [fontsLoaded] = useFonts({
     Merriweather_400Regular,
