e31c40a24eec5f47e83a6a62206190e2849347c1
ld50eli <elisey.minhairov@mail.ru>
2021-03-04 14:50:11 +0300

    Added pagination elements

--- src/organisms/CatalogView/index.tsx
+++ src/organisms/CatalogView/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView, TouchableOpacity } from 'react-native'
 import { useResource } from 'rest-hooks'
 import { useNavigation } from '@react-navigation/native'
@@ -7,14 +7,65 @@ import WineCard from '../../molecules/WineCard'
 import ROUTES from '../../routes'
 import { useFilters } from '../FiltersBar/FiltersContext'
 
+const POSITIONS_PER_PAGE = 5
+
 function CatalogView() {
   const { query } = useFilters()
 
+  const defaultState = {
+    layoutStats: {
+      xPosition: 0,
+      yPosition: 0,
+      layoutWidth: 50,
+      layoutHeight: 100,
+    },
+  }
+
+  const startingPageState = {
+    pageStats: {
+      pageNumber: 1,
+      firstItem: 0,
+      lastItem: POSITIONS_PER_PAGE - 1,
+    },
+  }
+
+  const [state, setState] = useState(defaultState)
+  const [page, setPage] = useState(startingPageState)
+
   const catalog = useResource(CatalogResource.filteredShape(), {
-    from: 0,
-    to: 5,
+    from: page.pageStats.firstItem,
+    to: page.pageStats.lastItem,
     searchParameters: query,
   })
+
+  const getLayoutStats = (event: any) => {
+    const { x, y, width, height } = event.nativeEvent.layout
+    const currentLayout = {
+      xPosition: x,
+      yPosition: y,
+      layoutWidth: width,
+      layoutHeight: height,
+    }
+
+    setState({ layoutStats: currentLayout })
+  }
+
+  const handleScroll = (event: any) => {
+    const yOffset = event.nativeEvent.contentOffset.y
+
+    if (yOffset > 0.6 * state.layoutStats.layoutHeight) {
+      const currentPage = {
+        pageNumber: page.pageStats.pageNumber + 1,
+        firstItem: page.pageStats.pageNumber * POSITIONS_PER_PAGE,
+        lastItem:
+          page.pageStats.pageNumber * POSITIONS_PER_PAGE +
+          POSITIONS_PER_PAGE -
+          1,
+      }
+
+      setPage({ pageStats: currentPage })
+    }
+  }
 
   const navigation = useNavigation()
 
@@ -25,7 +76,7 @@ function CatalogView() {
   }
 
   return (
-    <ScrollView>
+    <ScrollView onScroll={handleScroll} onLayout={getLayoutStats}>
       {catalog.map((position) => (
         <TouchableOpacity
           key={position.pk()}
