5105b3536dbcac1df00bd3fc0a006cff8212a99e
darya <dariy060100@mail.ru>
2021-03-11 19:26:49 +0300

    added .env and hide secret info

--- src/atoms/wineTitle/index.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react-native'
-import React from 'react'
-import WineCard from './index'
-
-describe('<WineCard />', () => {
-  it('should render', () => {
-    render(<WineCard />)
-  })
-})
