b37ed4acd304136fdf37945cd164bba2e6583513
darya <dariy060100@mail.ru>
2021-03-25 19:29:38 +0300

    favorites sreen for unregistered users

--- src/screens/Favourites.tsx
+++ src/screens/Favourites.tsx
@@ -1,12 +1,16 @@
-import { ScrollView } from 'react-native'
+import { ScrollView, Text } from 'react-native'
 import React, { Suspense } from 'react'
 import styled from 'styled-components/native'
 import WineCardLoader from '../molecules/WineCard/Loader'
 import SearchInput from '../molecules/SearchInput'
 import FavoritesToolbar from '../organisms/FavoritesToolbar'
 import FavoritesView from '../organisms/FavoritesView'
+import { useAuthContext } from './Auth/AuthContext'
+import UnauthorizedProfile from './UnauthorizedProfile'
 
 export default function FavouritesScreen() {
+  const { accessToken } = useAuthContext()
+
   const loader = (
     <ScrollView showsVerticalScrollIndicator={false}>
       <WineCardLoader />
@@ -18,8 +22,11 @@ export default function FavouritesScreen() {
   return (
     <Suspense fallback={loader}>
       <StyledSearchInput value='' />
-      <FavoritesToolbar handleClear={() => {}} />
-      <FavoritesView />
+      {accessToken ? (
+        [<FavoritesToolbar handleClear={() => {}} />, <FavoritesView />]
+      ) : (
+        <UnauthorizedProfile />
+      )}
     </Suspense>
   )
 }
