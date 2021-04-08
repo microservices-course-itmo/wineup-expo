7934a7cb1c983c359af04d758405c7293a8e4a5f
CleanBread <37740558+CleanBread@users.noreply.github.com>
2021-04-08 18:41:00 +0300

    fix catalog search input (#84)

--- src/screens/Catalog.tsx
+++ src/screens/Catalog.tsx
@@ -8,6 +8,8 @@ import WineCardLoader from '../molecules/WineCard/Loader'
 import { FiltersProvider } from '../organisms/FiltersBar/FiltersContext'
 
 export default function CatalogScreen() {
+  const [search, setSearch] = React.useState('')
+
   const loader = (
     <ScrollView showsVerticalScrollIndicator={false}>
       <WineCardLoader />
@@ -18,7 +20,7 @@ export default function CatalogScreen() {
 
   return (
     <FiltersProvider>
-      <StyledSearchInput value='' />
+      <StyledSearchInput value={search} onChange={setSearch} />
       <FiltersBar />
       <Suspense fallback={loader}>
         <CatalogView />
